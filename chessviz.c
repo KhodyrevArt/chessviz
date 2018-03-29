ef1349d2355b0fd9b8474f7c7170e4a8a0e0e7a3
Ходырев Артем Анатольевич <ip713s24@mwp06.csc.local>
2018-03-29 12:07:45 +0700

    add file chessviz.c

---
+++ chessviz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("  a b c d e f g h  \n");
+  printf("8 r n b q k b n r 8\n");
+  printf("7 p p p p p p p p 7\n");
+  printf("6                 6\n");
+  printf("5                 5\n");
+  printf("4                 4\n");
+  printf("3                 3\n");
+  printf("2 P P P P P P P P 2\n");
+  printf("1 R N B Q K B N R 1\n");
+  printf("  a b c d e f g h  \n");
+}
+  
